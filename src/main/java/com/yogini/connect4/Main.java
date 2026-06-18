2ec5f2e2212d1ff7ad12d920f5967c97b3669d02
Yogini Mori <yoginimori2003@gmail.com>
2026-06-18 19:00:34 +0530

    Connecting FXML to Java

--- src/main/java/com/yogini/connect4/Main.java
+++ src/main/java/com/yogini/connect4/Main.java
@@ -3,17 +3,27 @@ package com.yogini.connect4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+	private Controller controller;
+
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game.fxml"));
-		Scene scene = new Scene(fxmlLoader.load());
-		stage.setTitle("Hello!");
+		GridPane rootGridPane = fxmlLoader.load();
+
+		controller = fxmlLoader.getController();
+
+		Scene scene = new Scene(rootGridPane);
+
+		stage.setTitle("Connect 4");
 		stage.setScene(scene);
+		stage.setResizable(false);
 		stage.show();
 	}
 }
