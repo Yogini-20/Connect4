8778a0947cd9076e29e9355c61bdb2da2adf6c02
Yogini Mori <yoginimori2003@gmail.com>
2026-06-18 23:20:27 +0530

    Add menu bar

--- src/main/java/com/yogini/connect4/Main.java
+++ src/main/java/com/yogini/connect4/Main.java
@@ -3,7 +3,12 @@ package com.yogini.connect4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,7 +22,13 @@ public class Main extends Application {
 		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game.fxml"));
 		GridPane rootGridPane = fxmlLoader.load();
 
-		controller = fxmlLoader.getController();
+		controller = fxmlLoader.getController(); // those method used to pass fxml data to controller. In temperature app other method wee used to do that.
+
+		MenuBar menuBar = createMenu();
+		menuBar.prefWidthProperty().bind(stage.widthProperty()); // To cover all the space of the menuPane.
+
+		Pane menuPane = (Pane) rootGridPane.getChildren().get(0);
+		menuPane.getChildren().add(menuBar);
 
 		Scene scene = new Scene(rootGridPane);
 
@@ -25,5 +36,22 @@ public class Main extends Application {
 		stage.setScene(scene);
 		stage.setResizable(false);
 		stage.show();
+	}
+
+	private MenuBar createMenu() {
+
+		Menu fileMenu = new Menu("File");
+
+		MenuItem newGame = new MenuItem("New game");
+		MenuItem resetGame = new MenuItem("Reset game");
+		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+		MenuItem exitGame = new MenuItem("Exit game");
+
+		fileMenu.getItems().addAll(newGame, resetGame, separatorMenuItem, exitGame);
+
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu);
+
+		return menuBar;
 	}
 }
