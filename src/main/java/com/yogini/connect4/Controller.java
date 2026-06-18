2086a34df23f92e3bf2b16c876e890a35f978433
Yogini Mori <yoginimori2003@gmail.com>
2026-06-18 16:58:40 +0530

    layout

--- src/main/java/com/yogini/connect4/Controller.java
+++ src/main/java/com/yogini/connect4/Controller.java
@@ -1,11 +1,20 @@
 package com.yogini.connect4;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+	@FXML
+	public GridPane rootGridPane;
+	@FXML
+	public Pane insertedDiscsPane;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
