c7a4b4256f6719ec7a93c81f04249d4a87b7cadb
Yogini Mori <yoginimori2003@gmail.com>
2026-06-19 00:28:21 +0530

    Add actions

--- src/main/java/com/yogini/connect4/Main.java
+++ src/main/java/com/yogini/connect4/Main.java
@@ -1,14 +1,13 @@
 package com.yogini.connect4;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -43,17 +42,22 @@ public class Main extends Application {
 		Menu fileMenu = new Menu("File");
 
 		MenuItem newGame = new MenuItem("New game");
+		newGame.setOnAction(event -> resetGame());
 		MenuItem resetGame = new MenuItem("Reset game");
+		resetGame.setOnAction(event -> resetGame());
 		SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
 		MenuItem exitGame = new MenuItem("Exit game");
+		exitGame.setOnAction(event -> exitGame());
 
 		fileMenu.getItems().addAll(newGame, resetGame, separatorMenuItem, exitGame);
 
 		Menu helpMenu = new Menu("Help");
 
 		MenuItem aboutGame = new MenuItem("About Connect4");
+		aboutGame.setOnAction(event -> aboutConnect4());
 		SeparatorMenuItem separator = new SeparatorMenuItem();
 		MenuItem aboutMe = new MenuItem("About me");
+		aboutMe.setOnAction(event -> aboutMe());
 
 		helpMenu.getItems().addAll(aboutGame, separator, aboutMe);
 
@@ -61,5 +65,32 @@ public class Main extends Application {
 		menuBar.getMenus().addAll(fileMenu, helpMenu);
 
 		return menuBar;
+	}
+
+	private void aboutMe() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("About the developer");
+		alert.setHeaderText("Yogini Mori");
+		alert.setContentText("I love to play around with code and create games. Connect 4 is one of them. In free time i like to spend time with nears and dears.");
+		alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+		alert.showAndWait();
+	}
+
+	private void aboutConnect4() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("About Connect 4");
+		alert.setHeaderText("How to play?");
+		alert.setContentText("Connect Four is a two-player connection game in which the players first choose a color and then take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the next available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs. Connect Four is a solved game. The first player can always win by playing the right moves.");
+		alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+		alert.showAndWait();
+	}
+
+	private void exitGame() {
+		Platform.exit();
+		System.exit(0);
+	}
+
+	private void resetGame() {
+
 	}
 }
